9a3aa9610ef7ffb65f46a67a576fad4e9b1fbd3c
linzuxin <linzuxin@163.com>
2020-06-15 00:23:34 +0800

    rstring

--- src/unit/rstring.c
+++ src/unit/rstring.c
@@ -1,33 +1,28 @@
-#include <limits.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "rstring.h"
 
 /***判断字符串类型***/
 //size_t就是unsigned long long
 char structType(size_t string_size)
 {
-    xmRepeat(16,xmType,string_size);
-//32位系统下LONG_MAX占4字节，LLONG_MAX占8字节；
-//64位系统下LONG_MAX占8字节，LLONG_MAX占8字节；
-//故若为64位系统要区分类型是RSTRING_TYPE_32还是RSTRING_TYPE_64，32位系统不区分
-    #if (LONG_MAX == LLONG_MAX)
-        {
-            xmType(32,string_size);
-            return RSTRING_TYPE_64;
-        }
-    #else
-        return RSTRING_TYPE_32;
-    #endif
+    xmRepeat(16, xmType, string_size);
+    //32位系统下LONG_MAX占4字节，LLONG_MAX占8字节；
+    //64位系统下LONG_MAX占8字节，LLONG_MAX占8字节；
+    //故若为64位系统要区分类型是RSTRING_TYPE_32还是RSTRING_TYPE_64，32位系统不区分
+#if (LONG_MAX == LLONG_MAX)
+    {
+        xmType(32, string_size);
+        return RSTRING_TYPE_64;
+    }
+#else
+    return RSTRING_TYPE_32;
+#endif
 }
 
 /***根据类型判断动态字符串结构体的大小（不包含字符串内容）***/
 int HeaderSize(char type)
 {
     char currType = type & 7;
-    xmRepeat(64,xmHeader,currType);    
+    xmRepeat(64, xmHeader, currType);
     return 0;
 }
 
@@ -35,15 +30,17 @@ int HeaderSize(char type)
 size_t rstringAvail(pstr s)
 {
     char currType = structType(strlen(s));
-    xmRepeat(64,xmAvail,currType);
-    return 0;    
+    xmAvail5(5, currType);
+    xmRepeat(64, xmAvail, currType);
+    return 0;
 }
 
 /***获取字符串长度***/
 size_t rstringLen(pstr s)
-{    
+{
     char currType = structType(strlen(s));
-    xmRepeat(64,xmStringLen,currType);
+    xmStringLen5(5, currType);
+    xmRepeat(64, xmStringLen, currType);
     return 0;
 }
 
@@ -51,36 +48,42 @@ size_t rstringLen(pstr s)
 void rstringSet(pstr s, size_t newlen)
 {
     char currType = structType(strlen(s));
-    xmRepeat(64,xmStringSet,currType);    
+    if (currType == RSTRING_TYPE_5)
+    {
+        rstring5 *start = (rstring5 *)(s - sizeof(rstring5));
+        start->flags = RSTRING_TYPE_5 | (newlen << 3);
+        printf("len set result:%d\n", (start->flags >> 3));
+    }
+    xmRepeat(64, xmStringSet, currType);
 }
 
 /***创建字符串***/
 pstr rstringNewLen(const void *init, size_t initlen)
-{   
-    void *sh;//指向动态字符串结构体的的指针
-    pstr s;//指向动态字符串内容的指针
-    char type = structType(initlen);//获取动态字符串结构体的类型
+{
+    void *sh;                        //指向动态字符串结构体的的指针
+    pstr s;                          //指向动态字符串内容的指针
+    char type = structType(initlen); //获取动态字符串结构体的类型
     //若RSTRING_TYPE_5类型字符串为空，转换成RSTRING_TYPE_8类型
-    if(type == RSTRING_TYPE_5 && initlen == 0)
+    if (type == RSTRING_TYPE_5 && initlen == 0)
     {
         type = RSTRING_TYPE_8;
     }
-    int hdrlen = HeaderSize(type);//获取动态字符串结构体的头部大小
-    sh = malloc(hdrlen + initlen + 1);//给指向动态字符串结构体的头部的指针分配空间
-    if(sh == NULL)
+    int hdrlen = HeaderSize(type);     //获取动态字符串结构体的头部大小
+    sh = malloc(hdrlen + initlen + 1); //给指向动态字符串结构体的头部的指针分配空间
+    if (sh == NULL)
     {
         return NULL;
     }
-    if(!init)
+    if (!init)
     {
         //指针初始化
         memset(sh, 0, hdrlen + initlen + 1);
     }
     //计算指向动态字符串内容的指针的位置：
     //位置=动态字符串头部的指针地址+头部长度
-    s = (char*)sh + hdrlen;
-    xmRepeat(64,xmStringNewLen,type);
-    if(initlen && init)
+    s = (char *)sh + hdrlen;
+    xmRepeat(64, xmStringNewLen, type);
+    if (initlen && init)
     {
         //复制init中的内容到s中去。
         memcpy(s, init, initlen);
@@ -91,10 +94,9 @@ pstr rstringNewLen(const void *init, size_t initlen)
 
 pstr rstringNew(const char *init)
 {
-    size_t initlen = (init == NULL)? 0 : strlen(init);//获取动态字符串长度
+    size_t initlen = (init == NULL) ? 0 : strlen(init); //获取动态字符串长度
     return sdsnewlen(init, initlen);
 }
-
 
 /***为字符串重新分配容量***/
 pstr sdsMakeRoomFor(pstr s, size_t addlen)
@@ -110,77 +112,76 @@ pstr sdsMakeRoomFor(pstr s, size_t addlen)
     char type = structType(len);
     size_t newlen = (len + addlen);
     char newtype = structType(newlen);
-    void *sh, *newsh;
-    sh = (char*)s - HeaderSize(type);
+    void *sh = NULL, *newsh = NULL;
+    sh = (char *)s - HeaderSize(type);
     size_t newhdrlen = HeaderSize(newtype);
-    if(newtype == RSTRING_TYPE_5)
+    if (newtype == RSTRING_TYPE_5)
     {
         newtype = RSTRING_TYPE_8;
     }
     //类型相同则重新分配结构体空间即可
-    if(type == newtype)
+    if (type == newtype)
     {
         //结构体部分的位置不改变，但是柔型数组的长度改变
         newsh = realloc(sh, newhdrlen + newlen + 1);
-        printf("location of newsh: %p\n",newsh);
-        printf("location of sh: %p\n",sh);
-        if(newsh == NULL)
+        printf("location of newsh: %p\n", newsh);
+        printf("location of sh: %p\n", sh);
+        if (newsh == NULL)
         {
             return NULL;
         }
-        s = (char*)newsh + newhdrlen;
-        printf("content:%s\n",s);
+        s = (char *)newsh + newhdrlen;
+        printf("content:%s\n", s);
     }
     else
     {
         //结构体部分的位置和柔型数组的长度都改变
         newsh = malloc(newhdrlen + newlen + 1);
-        printf("location of newsh: %p\n",newsh);
-        printf("location of sh: %p\n",sh);
-        if(newsh == NULL)
+        printf("location of newsh: %p\n", newsh);
+        printf("location of sh: %p\n", sh);
+        if (newsh == NULL)
         {
             return NULL;
         }
-        memcpy((char*)newsh + newhdrlen, s, len + 1);
+        memcpy((char *)newsh + newhdrlen, s, len + 1);
         free(sh);
-        printf("location of sh: %p\n",sh);
-        s = (char*)newsh + newhdrlen;
-        printf("content:%s\n",s);
+        printf("location of sh: %p\n", sh);
+        s = (char *)newsh + newhdrlen;
+        printf("content:%s\n", s);
     }
-    return s;    
+    return s;
 }
 
 /***拼接字符串***/
-pstr sdscatlen(pstr s, const void *t, size_t addlen) 
+pstr sdscatlen(pstr s, const void *t, size_t addlen)
 {
     size_t currlen = rstringLen(s);
-    s = sdsMakeRoomFor(s,addlen);
+    s = sdsMakeRoomFor(s, addlen);
     if (s == NULL)
     {
         return NULL;
-    } 
-    memcpy(s+currlen, t, addlen);
+    }
+    memcpy(s + currlen, t, addlen);
     rstringSet(s, currlen + addlen);
     s[currlen + addlen] = '\0';
     return s;
 }
 
-pstr sdscat(pstr s, const char *t) 
+pstr sdscat(pstr s, const char *t)
 {
     return sdscatlen(s, t, strlen(t));
 }
 
-
 /***释放字符串***/
 void sdsfree(pstr s)
 {
-    if(s == NULL)
+    if (s == NULL)
     {
         return;
     }
     char type = structType(strlen(s));
     int hdrSize = HeaderSize(type);
-    void *sh = s-hdrSize;
+    void *sh = s - hdrSize;
     free(sh);
     sh = NULL;
 }
--- src/unit/rstring.h
+++ src/unit/rstring.h
@@ -8,7 +8,7 @@
 #include <string.h>
 
 //指向字符串文本内容的指针
-typedef char * pstr;
+typedef char *pstr;
 typedef uint8_t xmbyte1;
 typedef uint16_t xmbyte2;
 typedef uint32_t xmbyte4;
@@ -22,7 +22,7 @@ typedef uint64_t xmbyte8;
 #define RSTRING_TYPE_32 3
 #define RSTRING_TYPE_64 4
 
-size_t gAlloc[] = {0,255,65535,4294967295,18446744073709551615};
+size_t gAlloc[] = {0, 255, 65535, 4294967295, 18446744073709551615};
 #define xmRepeat(n, m, p) xmAppend(xmRepeat_, n)(m, p)
 #define xmAppend(x, y) x##y
 #define xmRepeat_0(m, p)
@@ -33,103 +33,92 @@ size_t gAlloc[] = {0,255,65535,4294967295,18446744073709551615};
 #define xmRepeat_64(m, p) xmRepeat_32(m, p) m(64, p)
 
 //宏：rstring结构体
-#define rstringStruct(a,b) rstring##a{\
-        xmbyte##b len;\
-        xmbyte##b alloc;\
-        unsigned char flags;\
-        char buf[];\
+#define rstringStruct(a, b)  \
+    typedef struct           \
+    {                        \
+        xmbyte##b len;       \
+        xmbyte##b alloc;     \
+        unsigned char flags; \
+        char buf[];          \
+    } rstring##a
+
+#define xmType(n, string_size)   \
+    if (string_size < 1 << n)    \
+    {                            \
+        return RSTRING_TYPE_##n; \
     }
 
-#define xmType(n,string_size) if(string_size < 1 << n)\
-    {\
-        return RSTRING_TYPE_##n;\
+#define xmRstringTypeEqual(n, p, f) \
+    if (##p == RSTRING_TYPE_##n)    \
+    {                               \
+        ##fFunc(n);                 \
     }
 
-#define xmHeader(n,p) if(##p == RSTRING_TYPE_##n)\
-    {\
-        return sizeof(struct rstring##n);\
-    }
+#define xmRstringStructStart(n) \
+    rstring##n *start = (rstring##n *)(s - sizeof(rstring##n))
 
-#define xmAvail(n,p) if(##p==RSTRING_TYPE_##n)\
-    {\
-        struct rstring##n *start = (struct rstring##n*)(s - sizeof(struct rstring##n));\
-        size_t avail;\
-        if(##p==RSTRING_TYPE_5)\
-        {\
-            avail = 31 - ((start->flags) >> 3);\
-        }\
-        else\
-        {\
-            avail = start->alloc - start->len;\
-        }\
-        return avail;\
-    }
+#define xmHeaderFunc(n, p) return sizeof(rstring##n);
 
-#define xmStringLen(n,p) if(##p==RSTRING_TYPE_##n)\
-    {\
-        struct rstring##n *start = (struct rstring##n *)(s - sizeof(struct rstring##n));\
-        size_t length;\
-        if(##p==RSTRING_TYPE_5)\
-        {\
-            length = (start->flags) >> 3;\
-        }\
-        else\
-        {\
-            length = start->len;\
-        }\
-        return length;\
-    }
-    
-#define xmStringSet(n,p) if(##p==RSTRING_TYPE_##n)\
-    {\
-        struct rstring##n *start = (struct rstring##n*)(s - sizeof(struct rstring##n));\
-        if(##p==RSTRING_TYPE_5)\
-        {\
-            start->flags = RSTRING_TYPE_5 | (newlen << 3);\
-            printf("len set result:%d\n", (start->flags >> 3));\
-        }\
-        else\
-        {\
-            int order = ##n/8;\
-            start->len = newlen;\
-            start->alloc = gAlloc[order];\
-            printf("len set result:%d\n", start->len);\
-            printf("alloc set result:%ld\n", start->alloc);\   
-        }\
-    }
+#define xmAvail5Func(n)                        \
+    xmRstringStructStart(n);                   \
+    size_t avail = 31 - ((start->flags) >> 3); \
+    return avail;
 
-#define xmStringNewLen(n,p) if(##p==RSTRING_TYPE_##n)\
-    {\
-        struct rstring##n *start = (struct rstring##n*)(s - sizeof(struct rstring##n));\
-        if(##p==RSTRING_TYPE_5)\
-        {\
-            start->flags = RSTRING_TYPE_5 | (initlen << 3);\
-        }\
-        else\
-        {\
-            int order = ##n/8;\
-            start->len = initlen;\
-            start->alloc = gAlloc[order];\
-            start->flags = type;\
-        }\
-    }
+#define xmAvailFunc(n)                        \
+    xmRstringStructStart(n);                  \
+    size_t avail = start->alloc - start->len; \
+    return avail;
 
+#define xmStringLenFunc(n)      \
+    xmRstringStructStart(n);    \
+    size_t length = start->len; \
+    return length;
 
+#define xmStringLen5Func(n)              \
+    xmRstringStructStart(n);             \
+    size_t length = (start->flags) >> 3; \
+    return length;
 
+#define xmStringSetFunc(n)   \
+    xmRstringStructStart(n); \
+    int order = ##n / 8;     \
+    start->len = newlen;     \
+    start->alloc = gAlloc[order];
+
+#define xmStringNewLenFunc(n)     \
+    xmRstringStructStart(n);      \
+    int order = ##n / 8;          \
+    start->len = initlen;         \
+    start->alloc = gAlloc[order]; \
+    start->flags = type;
+
+#define xmHeader(n, p) xmRstringTypeEqual(n, p, xmHeader)
+#define xmStringLen(n, p) xmRstringTypeEqual(n, p, xmStringLen)
+#define xmStringLen5(n, p) xmRstringTypeEqual(n, p, xmStringLen5)
+#define xmAvail(n, p) xmRstringTypeEqual(n, p, xmAvail)
+#define xmAvail5(n, p) xmRstringTypeEqual(n, p, xmAvail5)
+#define xmStringSet(n, p) xmRstringTypeEqual(n, p, xmStringSet)
+#define xmStringNewLen(n, p) xmRstringTypeEqual(n, p, xmStringNewLen)
 //定义结构体
 //5位字符串结构体（字符串大小为2^5-1以内）
-struct rstring5
+typedef struct
 {
     unsigned char flags; //前三位类型，后五位长度
     char buf[];          //存储字符串文本内容的数组
-};
+} rstring5;
+
 //8位字符串结构体（字符串大小为2^8-1以内）
-struct rstringStruct(8,1);
+rstringStruct(8, 1);
 //16位字符串结构体（字符串大小为2^16-1以内）
-struct rstringStruct(16,2);
+rstringStruct(16, 2);
 //32位字符串结构体（字符串大小为2^32-1以内）
-struct rstringStruct(32,4);
+rstringStruct(32, 4);
 //64位字符串结构体（字符串大小为2^64-1以内）
-struct rstringStruct(64,8);
+rstringStruct(64, 8);
+
+size_t rstringAvail(pstr s);
+size_t rstringLen(pstr s);
+void rstringSet(pstr s, size_t newlen);
+pstr rstringNew(const char *init);
 
 #endif
